3c59cbeea1b1acd6c023728aef0a498656627324
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2019-10-02 19:02:47 -0400

    changed key name and removed questions section

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,15 +1,8 @@
 import React, { useEffect } from 'react'
 import { useLocalStorage } from './useLocalStorage'
 
-// QUESTIONS
-  // WHERE IS THE KEY 'FALSE' COMING FROM IN THE APPLICATION LOCAL STORAGE?
-  // WHAT IS THE POINT OF KEY HERE (LINES 8 AND 9)?
-  // WHY DID I HAVE TO PUT ALL OF LINE 9?
-  // WHAT IS LINE 13'S STOREDVALUE ACTUALLY CHANGING?
-  // OVERALL, HOW ARE THESE HOOKS WORKING TOGETHER?
-  
 export const useDarkMode = (key) => {
-  const [storedValue, setValue] = useLocalStorage(key)
+  const [storedValue, setValue] = useLocalStorage(key, false)
 
   useEffect(() => {
     storedValue === true ? document.querySelector('body').classList.add('dark-mode') : document.querySelector('body').classList.remove('dark-mode')
