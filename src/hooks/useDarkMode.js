8dfa4d36211939ee15181563258efa63e6c6eb9d
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2019-10-02 16:18:01 -0400

    finished adding dark mode to page which is stored in local storage

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,7 +1,14 @@
 import React, { useEffect } from 'react'
-import useLocalStorage from './useLocalStorage'
+import { useLocalStorage } from './useLocalStorage'
 
-export const useDarkMode = () => {
+// QUESTIONS
+  // WHERE IS THE KEY 'FALSE' COMING FROM IN THE APPLICATION LOCAL STORAGE?
+  // WHAT IS THE POINT OF KEY HERE (LINES 8 AND 9)?
+  // WHY DID I HAVE TO PUT ALL OF LINE 9?
+  // WHAT IS LINE 13'S STOREDVALUE ACTUALLY CHANGING?
+  // OVERALL, HOW ARE THESE HOOKS WORKING TOGETHER?
+  
+export const useDarkMode = (key) => {
   const [storedValue, setValue] = useLocalStorage(key)
 
   useEffect(() => {
